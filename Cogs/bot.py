8b146fee489ec340a8174077959dd19d201fe625
Rehman Ali <rehmanali.9442289@gmail.com>
2021-03-04 18:45:23 +0500

    Update

--- Cogs/bot.py
+++ Cogs/bot.py
@@ -22,10 +22,6 @@ log = logging.getLogger('DiscordBot').info
 async def on_ready():
     print('[+] We have logged in as {0.user}'.format(bot))
     print('[+] Loading extensions ...')
-    channel_id = config.get("INNVICTUS_CHANNEL_ID")
-    innvictus_channel = discord.utils.get(
-        bot.guilds[0].channels, id=channel_id)
-    await innvictus_channel.send('Now online!')
     extensions = ['innvictus_commands']
     for ext in extensions:
         try:
